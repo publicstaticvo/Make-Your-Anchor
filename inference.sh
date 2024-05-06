88d4322417bd4b8ce8fab22a8d726e299d813411
publicstaticvo <42710459+publicstaticvo@users.noreply.github.com>
2024-05-06 15:40:02 +0800

    Update inference.sh

--- inference.sh
+++ inference.sh
@@ -1,18 +1,16 @@
 cd inference
 
-export CUDA_VISIBLE_DEVICES=4
-
 ## Please fill the parameters here
 # path to the body model folder
-body_weight_dir=./checkpoints/seth/body
+body_weight_dir=/content/drive/MyDrive/make-your-anchor/checkpoints/seth/body
 # path to the head model folder
-head_weight_dir=./checkpoints/seth/head
+head_weight_dir=/content/drive/MyDrive/make-your-anchor/checkpoints/seth/head
 # path to the input poses
-body_input_dir=./samples/poses/seth1
+body_input_dir=/content/mya/inference/seth/poses/seth1
 # path to the reference body appearance
-body_prompt_img_pth=./samples/appearance/body.png
+body_prompt_img_pth=/content/mya/inference/seth/appearance/body.png
 # path to the reference head appearance
-head_prompt_img_pth=./samples/appearance/head.png
+head_prompt_img_pth=/content/mya/inference/seth/appearance/head.png
 
 # pipe_path=runwayml/stable-diffusion-v1-5
 # pipe_inpainting_path=runwayml/stable-diffusion-inpainting
@@ -27,8 +25,7 @@ head_controlnet_path=${head_weight_dir}/controlnet
 head_cfg=3.5
 head_condition_scale=1.
 
-save_root=./samples/output
-
+save_root=/content/mya/inference/seth/output
 
 fps=30
 
@@ -39,57 +36,25 @@ head_mask_dir=${body_headcrop_root}/raw_aligned_mask
 head_input_dir=${body_headcrop_root}/aligned
 head_matrix_dir=${body_headcrop_root}/matrix
 head_save_dir=${body_headcrop_root}/face_output
-
 final_save_dir=${save_root}/final_output
 
 # inference on body
-python inference_body.py \
-    --unet_path $body_unet_path \
-    --controlnet_path $body_controlent_path \
-    --input_dir $body_input_dir \
-    --save_dir $body_save_dir \
-    --prompt_img_pth $body_prompt_img_pth \
-    --CFG $body_cfg \
-    --condition_scale $body_condition_scale \
-    # --pipe_path $pipe_path
+python inference_body.py --unet_path $body_unet_path --controlnet_path $body_controlent_path --input_dir $body_input_dir --save_dir $body_save_dir --prompt_img_pth $body_prompt_img_pth --CFG $body_cfg --condition_scale $body_condition_scale # --pipe_path $pipe_path
 
 ffmpeg -r 30 -i $body_save_dir/%06d.png -q:v 0 -pix_fmt yuv420p $body_save_dir.mp4
 
 # face alignment
-python face_alignment.py \
-    --imgdir_pth $body_input_dir \
-    --raw_imgdir_pth $body_save_dir \
-    --results_dir $body_headcrop_root \
-    --crop_size 512
+python face_alignment.py --imgdir_pth $body_input_dir --raw_imgdir_pth $body_save_dir --results_dir $body_headcrop_root --crop_size 512
 
 # get face mask
-python get_mask.py \
-    --input_pth $head_dir \
-    --mask_pth $head_mask_dir
-
+python get_mask.py --input_pth $head_dir --mask_pth $head_mask_dir
 
 # inference on face inpainting
-python inference_face_inpainting.py \
-    --unet_path $head_unet_pth \
-    --controlnet_path $head_controlnet_path \
-    --input_dir $head_input_dir \
-    --face_dir $head_dir \
-    --mask_dir $head_mask_dir \
-    --save_dir $head_save_dir \
-    --prompt_img_pth $head_prompt_img_pth \
-    --CFG $head_cfg \
-    --condition_scale $head_condition_scale \
-    --batch_size 30 \
+python inference_face_inpainting.py --unet_path $head_unet_pth --controlnet_path $head_controlnet_path --input_dir $head_input_dir --face_dir $head_dir --mask_dir $head_mask_dir --save_dir $head_save_dir --prompt_img_pth $head_prompt_img_pth --CFG $head_cfg --condition_scale $head_condition_scale --batch_size 30 \
     # --pipe_pth $pipe_inpainting_path
 
-
 # face blending
-python face_blending.py \
-    --body_dir $body_save_dir \
-    --face_dir $head_save_dir \
-    --matrix_dir $head_matrix_dir \
-    --save_dir $final_save_dir \
-    --crop_size 512 
+python face_blending.py --body_dir $body_save_dir --face_dir $head_save_dir --matrix_dir $head_matrix_dir --save_dir $final_save_dir --crop_size 512
 
 
 ffmpeg -r 30 -i $final_save_dir/%06d.png -q:v 0 -pix_fmt yuv420p $final_save_dir.mp4
